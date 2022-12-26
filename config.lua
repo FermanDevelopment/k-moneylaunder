e20b60c4438c4f3b79db66caec69574c3f7ee244
Ferman Development <34038836+FermanDevelopment@users.noreply.github.com>
2022-12-26 19:13:15 +0300

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,6 @@
+Config = {
+    UseTarget = true,
+    location = vector4(195.17, -933.77, 30.7, 144.5),
+    item = false, -- Nakit Yerine Eşya İsmi Ayarlayabilirsiniz
+    value = 1 -- İşaretli Para Başına Kaç Dolar Ödeyeceğini Belirleyin
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+lua54 'yes'
+fx_version 'Ferman Development'
+game 'gta5'
+
+name 'K-Moneytrade'
+description 'Simple Marked Money Trading Ped Made For: Dj Ferman#0001'
+author 'Ferman Development'
+
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    '@PolyZone/client.lua',
+    '@PolyZone/CircleZone.lua',
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
